1828f6403fc329e7fb21598f691b596968190e3a
fariasdoto <tomasfariassantana@gmail.com>
2019-01-12 20:30:02 -0300

    Now creates dict of lists recursively, added helper param

--- converter.py
+++ converter.py
@@ -8,8 +8,8 @@ from openpyxl import Workbook
 
 class Converter:
     """
-        Converts JSON file to Excel or CSV file. Nested structures are flattened
-        by recursively appending lists of values.
+    Converts JSON file to Excel or CSV file. Nested structures are flattened
+    by recursively appending lists of values.
     """
 
     def __init__(self, file=None, export_format='csv', name='result'):
@@ -52,27 +52,29 @@ class Converter:
         with p.open(encoding='utf-8') as f:
             self._file = json.load(f)
 
-    def convert(self, csv_sep=','):
+    def convert(self, csv_sep=',', helper=None):
         """
         Converts JSON file to either Excel or CSV file, depending on
         self.csv state. File is saved to self.name
 
         Arguments:
             csv_sep (str): CSV delimiter, for example ',' or ';'.
+            helper (dict): See recursive_dict_of_lists.
         """
-        long_list = zip_longest(*recursive_list_of_lists(self.file))
+        flattened = recursive_dict_of_lists(self.file, helper)
         filename = f'{self.name}.{self.export_format}'
 
         if self.export_format == 'csv':
             with open(filename, 'w', newline='') as csvfile:
                 writer = csv.writer(csvfile, delimiter=csv_sep)
-                for l in long_list:
-                    writer.writerow(l)
+                writer.writerow(flattened.keys())
+                writer.writerows(zip_longest(*flattened.values()))
 
         elif self.export_format == 'xlsx':
             wb = Workbook()
             ws = wb.active
-            for row in long_list:
+            ws.append((_ for _ in flattened.keys()))  # doesn't take dict_keys ...
+            for row in zip_longest(*flattened.values()):
                     ws.append(row)
             wb.save(filename)
 
@@ -80,54 +82,51 @@ class Converter:
             raise ValueError(f"Unsupported export_format {self.export_format}. Use 'csv' or 'xlsx'")
 
 
-def recursive_keys(d):
-    keys = []
-    for k in d.keys():
-        if isinstance(d[k], dict):
-            keys += recursive_keys(d[k])
+def recursive_dict_of_lists(d, helper=None, prev_key=None):
+    """
+    Builds dictionary of lists by recursively traversing a JSON-like
+    structure.
+
+    Arguments:
+        d (dict): JSON-like dictionary.
+        prev_key (str): Prefix used to create dictionary keys like: prefix_key.
+            Passed by recursive step, not intended to be used.
+        helper (dict): In case d contains nested dictionaries, you can specify
+            a helper dictionary with 'key' and 'value' keys to specify where to
+            look for keys and values instead of recursive step. It helps with
+            cases like: {'action': {'type': 'step', 'amount': 1}}, by passing
+            {'key': 'type', 'value': 'amount'} as a helper you'd get
+            {'action_step': [1]} as a result.
+    """
+    d_o_l = {}
+
+    if helper is not None and helper['key'] in d.keys() and helper['value'] in d.keys():
+        if prev_key is not None:
+            key = f"{prev_key}_{helper['key']}"
         else:
-            keys.append(k)
-    return keys
+            key = helper['key']
 
+        if key not in d_o_l.keys():
+            d_o_l[key] = []
+        d_o_l[key].append(d[helper['value']])
 
-def recursive_values(d):
-    values = []
-    for k in d.keys():
-        if isinstance(d[k], dict):
-            values += recursive_values(d[k])
-        else:
-            values.append(d[k])
-    return values
+        return d_o_l
 
-
-def recursive_dict(d, prev_key=None):
-    dic = {}
-    for k in d.keys():
-        if isinstance(d[k], dict):
-            dic.update(recursive_dict(d[k], prev_key=k))
-        else:
-            if prev_key is not None:
-                new_key = f'{prev_key}_{k}'
-            else:
-                new_key = k
-            dic[new_key] = d[k]
-    return dic
-
-
-def recursive_list_of_lists(d, prev_key=None):
-    list_ = []
-    for k in d.keys():
-        if isinstance(d[k], dict):
-            list_.extend(recursive_list_of_lists(d[k], prev_key=k))
+    for k, v in d.items():
+        if isinstance(v, dict):
+            d_o_l.update(recursive_dict_of_lists(v, helper=helper, prev_key=k))
         else:
             if prev_key is not None:
-                new_list = [f'{prev_key}_{k}']
+                key = f'{prev_key}_{k}'
             else:
-                new_list = [k]
-            if isinstance(d[k], list):
-                for v in d[k]:
-                    new_list.append(v)
+                key = k
+
+            if key not in d_o_l.keys():
+                d_o_l[key] = []
+
+            if isinstance(v, list):
+                d_o_l[key].extend(v)
             else:
-                new_list.append(d[k])
-            list_.append(new_list)
-    return list_
+                d_o_l[key].append(v)
+
+    return d_o_l
--- tests/test_converter.py
+++ tests/test_converter.py
@@ -33,6 +33,31 @@ def file_path():
             continue
 
 
+@pytest.fixture(scope='module')
+def file_path_help():
+    d = {
+        'Test': 'Hi',
+        'Numbers!': [1, 2, 3],
+        'Hell': 'Yeah!',
+        'Will this work': {
+            'Hey': [1, 2]
+        },
+        'Dict help': {'key': 'key', 'value': 1, 'random_value': 'woop'}
+    }
+    p = Path('test.json')
+    with open(p, 'w') as fp:
+        json.dump(d, fp)
+
+    yield p
+
+    files = [Path('result.csv'), Path('result.xlsx'), p]
+    for file in files:
+        try:
+            file.unlink()
+        except FileNotFoundError:
+            continue
+
+
 def test_export_to_csv(file_path):
     with Converter(file_path, 'csv', 'result') as c:
         c.convert()
@@ -73,5 +98,63 @@ def test_export_to_xlsx(file_path):
             assert row == [None, 'Bye bye', 2, None, 2]
         if line_count == 3:
             assert row == [None, None, 3, None, None]
+
+        line_count += 1
+
+
+def test_export_to_csv_with_helper(file_path_help):
+    helper = {
+        'key': 'key',
+        'value': 'value'
+    }
+    with Converter(file_path_help, 'csv', 'result') as c:
+        c.convert(helper=helper)
+
+    with open('result.csv', 'r') as fp:
+        csv_reader = csv.reader(fp, delimiter=',')
+
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                assert row == [
+                    'Test', 'Numbers!', 'Hell', 'Will this work_Hey',
+                    'Dict help_key'
+                ]
+            if line_count == 1:
+                assert row == ['Hi', '1', 'Yeah!', '1', '1']
+            if line_count == 2:
+                assert row == ['', '2', '', '2', '']
+            if line_count == 3:
+                assert row == ['', '3', '', '', '']
+
+            line_count += 1
+
+
+def test_export_to_xlsx_with_helper(file_path_help):
+    helper = {
+        'key': 'key',
+        'value': 'value'
+    }
+    with Converter(file_path_help, 'xlsx', 'result') as c:
+        c.convert(helper=helper)
+
+    wb = load_workbook(filename='result.xlsx')
+    ws = wb.active
+
+    line_count = 0
+    for c1, c2, c3, c4, c5 in ws[ws.dimensions]:
+        row = [c1.value, c2.value, c3.value, c4.value, c5.value]
+
+        if line_count == 0:
+            assert row == [
+                'Test', 'Numbers!', 'Hell', 'Will this work_Hey',
+                'Dict help_key'
+            ]
+        if line_count == 1:
+            assert row == ['Hi', 1, 'Yeah!', 1, 1]
+        if line_count == 2:
+            assert row == [None, 2, None, 2, None]
+        if line_count == 3:
+            assert row == [None, 3, None, None, None]
 
         line_count += 1
